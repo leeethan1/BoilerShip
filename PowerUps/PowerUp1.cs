551c3b30a0c83b0756eb673aef29a09024273ed2
Umang Sharma <54654783+UmangSharma24@users.noreply.github.com>
2019-09-14 20:45:47 -0400

    Add files via upload

---
+++ PowerUps/PowerUp1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp1 : MonoBehaviour
+{
+    public float multiplier = 2f;
+
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter2D(Collider2D other)  /* other = Player */
+    {
+        if (other.CompareTag("Player"))            /* when player's tag collides with the denpop, trigger the effect */
+        {
+            Pickup(other);      /* Declares Pickup1 as a function */
+        }
+    }
+    void Pickup(Collider2D player)
+    {
+        // Add effect
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        // Remove from scene
+        player.transform.localScale *= multiplier;
+
+        // Spawn particle effect
+        Destroy(gameObject);
+
+
+
+    }
+}
+
